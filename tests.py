6e0af87a863f3208d2f89b4ced7b6f52bf0f432c
Fabio Di Lauro <fabio.dilauro@gmail.com>
2021-05-20 14:07:40 +0200

    added sleep between tests

--- tests.py
+++ tests.py
@@ -2,43 +2,52 @@ import unittest
 import subprocess
 from flag import *
 import base64
+import time
 
 class TestClients(unittest.TestCase):
     def test1(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_1.py']).decode().strip()
         self.assertTrue(check_flag_validity(1, output))
     
     def test2(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_2.py']).decode().strip()
         self.assertTrue(check_flag_validity(2, output))
     
     def test3(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_3.py']).decode().strip().split()
         self.assertEqual(output[0], 'random')
         self.assertEqual(output[1], 'port:')
         self.assertTrue(check_flag_validity(3, output[3]))
 
     def test4(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_4.py']).decode().strip().split()
         self.assertEqual(len(output), 6)
         self.assertTrue(check_flag_validity(4, output[5]))
 
     def test5(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_5.py']).decode().strip().split()
         self.assertEqual(len(output), 8)
         self.assertTrue(check_flag_validity(5, output[7]))
 
     def test6(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_6.py']).decode().strip().split()
         self.assertEqual(len(output), 9)
         self.assertTrue(check_flag_validity(6, output[8]))
 
     def test7(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_7.py']).decode().strip().split()
         self.assertEqual(len(output), 13)
         self.assertTrue(check_flag_validity(7, output[12]))
 
     def test8(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_8.py']).decode().strip().split()
         self.assertEqual(len(output), 35)
         self.assertEqual(output[0].upper(), 'GET')
@@ -47,6 +56,7 @@ class TestClients(unittest.TestCase):
         self.assertTrue(check_flag_validity(8, output[33]))
 
     def test9(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_9.py']).decode().strip().split()
         self.assertEqual(len(output), 25)
         self.assertEqual(output[0].upper(), 'POST')
@@ -55,6 +65,7 @@ class TestClients(unittest.TestCase):
         self.assertTrue(check_flag_validity(9, base64.b64decode(output[-1]).decode().strip()))
 
     def test10(self):
+        time.sleep(1)
         output = subprocess.check_output(['python', '../client_10.py']).decode().strip().split()
         self.assertEqual(len(output), 4)
         self.assertTrue(check_flag_validity(10, output[-1]))
